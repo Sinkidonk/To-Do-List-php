20b6b6d80525279eb0e115cc6e3fc9eadbc24c43
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-30 12:22:09 -0500

    Revert "upgraded security a bit"
    This reverts commit 52b4a01a500c403e131b0290a8a2ed355b037d21.

--- includes/pdo_connect.php
+++ includes/pdo_connect.php
@@ -1,17 +1,8 @@
 <?php
 
-class MyPDO extends PDO
-{
-    public function __construct($file = 'my_setting.ini')
-    {
-        if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
-       
-        $dns = $settings['database']['driver'] .
-        ':host=' . $settings['database']['host'] .
-        ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
-        ';dbname=' . $settings['database']['schema'];
-       
-        parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
-    }
+try {
+    $db = new PDO('mysql:host=localhost;dbname=parysa2_AdvPhpFinal', '', ''); /* username and password removed */
+    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $ex) {
+    echo "<p>$ex</p>";
 }
-$db = new MyPDO;
