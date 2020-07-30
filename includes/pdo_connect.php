52b4a01a500c403e131b0290a8a2ed355b037d21
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-30 12:09:26 -0500

    upgraded security a bit

--- includes/pdo_connect.php
+++ includes/pdo_connect.php
@@ -1,8 +1,17 @@
 <?php
 
-try {
-    $db = new PDO('mysql:host=localhost;dbname=parysa2_AdvPhpFinal', '', ''); /* username and password removed */
-    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $ex) {
-    echo "<p>$ex</p>";
+class MyPDO extends PDO
+{
+    public function __construct($file = 'my_setting.ini')
+    {
+        if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
+       
+        $dns = $settings['database']['driver'] .
+        ':host=' . $settings['database']['host'] .
+        ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
+        ';dbname=' . $settings['database']['schema'];
+       
+        parent::__construct($dns, $settings['database']['username'], $settings['database']['password']);
+    }
 }
+$db = new MyPDO;
